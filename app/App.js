eaeb97cceb1acc959e086ca7101ec9a64d376d95
Arbdoul <salisuabdullahi64@gmail.com>
2024-01-27 16:42:03 +0100

    install navitations dependences

--- app/App.js
+++ app/App.js
@@ -3,18 +3,13 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 item-center justify-center">
+      <Text className="text-blue-400">
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
